220bb3e316b2647f54ef77368931fc2e64dde8fc
enk <enkh.mon@gmail.com>
2019-10-26 11:47:14 +0800

    tabs added

--- dialogs-rep.py
+++ dialogs-rep.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 import codecs
@@ -8,10 +9,22 @@ myFileTypes = [
     ("All type", "*")
 ]
 
+
+
+# openFiles = [
+#     {
+#         filePath: "",
+#         tabId: ""
+#     }
+# ]
+
+
+
 currentFile = ""
+currentFileName = ""
 
 def selectFile():
-    global currentFile
+    global currentFile, currentFileName, window
     filepath = filedialog.askopenfilename(filetypes=myFileTypes)
     # messagebox.showinfo("Selected file name",filepath)
     if(filepath != ""):
@@ -19,6 +32,8 @@ def selectFile():
         content = ""
         lines = fileHandler.readlines()
         currentFile = filepath
+        currentFileName = filepath
+        window.title(currentFileName)
         print("SET Current file: " + currentFile)
         for line in lines:
             content = content + line
@@ -49,7 +64,14 @@ def selectFolder():
 def closeFile():
     global currentFile
     currentFile = ""
+    currentFileName = ""
+    window.title("Tk")
     textEntry.delete("1.0", "end")
+
+def addTab():
+    global notebook, window
+    frame = Frame(window)
+    notebook.add(frame, text='New tab')
 
 window = Tk()
 
@@ -62,9 +84,26 @@ fileMenu.add_command(label = "Save file", command = saveFile)
 fileMenu.add_command(label = "Select folder", command = selectFolder)
 fileMenu.add_command(label = "Close file", command = closeFile)
 
+fileMenu.add_command(label = "Test add tab", command = addTab)
+
 window["menu"] = menubar
 
-textEntry = Text(window, height = 10, width = 80)
-textEntry.grid(row = 0, column = 0, sticky = "nwes")
+# textEntry.grid(row = 0, column = 0, sticky = "nwes")
+
+notebook = ttk.Notebook(window)
+f1 = Frame(notebook)
+f2 = Frame(notebook)
+notebook.add(f1, text='Tab 1')
+notebook.add(f2, text='Tab 2')
+
+textEntry = Text(f1, height = 10, width = 80)
+textEntry.pack()
+
+notebook.pack()
+
+print(notebook.tabs())
+
+notebook.tab(".!notebook.!frame2", state="disabled")
+print(notebook.tab(".!notebook.!frame2"))
 
 window.mainloop()
