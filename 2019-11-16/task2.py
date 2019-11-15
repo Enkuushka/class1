08eec6aa06e86ae3e1bd90c2530fae74066fc274
enkh <enkh.mon@gmail.com>
2019-11-16 04:35:52 +0800

    u done

--- 2019-11-16/task2.py
+++ 2019-11-16/task2.py
@@ -22,8 +22,31 @@ def executeSql(query):
 
 songolt_1 = input("Унших[u], Бичих[b]: ")
 
-if(songolt_1 == "u"):
-    
+if(songolt_1 == "u"): 
+
+    query = '''SELECT id, title FROM content
+    ORDER BY created_date DESC
+    LIMIT 5'''
+
+    data = executeSql(query)
+    for row in data:
+        print("[%d] %s"%(row[0], row[1]))
+
+    content_id = int(input("Мэдээний ID: "))
+
+    query = '''SELECT * FROM content
+    WHERE id = %d'''%(content_id)
+
+    news = executeSql(query)
+    old_viewed_count = news[0][5]
+    query = '''UPDATE content
+    SET viewed_count = %d
+    WHERE id = %d'''%(old_viewed_count+1, content_id)
+    executeSql(query)
+    print(news[0][1])
+    print("--------------")
+    print(news[0][2])
+
     pass
 
 elif(songolt_1 == "b"):
@@ -45,7 +68,10 @@ elif(songolt_1 == "b"):
     body = input("Мэдээ: ")
 
     max_id = executeSql("SELECT MAX(id) FROM content")
-    new_id = max_id[0][0] + 1
+    if(max_id == None):
+        new_id = 1
+    else:
+        new_id = max_id[0][0] + 1
 
     insertQuery = '''
         INSERT INTO content (id, title, body, worker_id, category_id, created_date) 
