5f876bf62f1566a41800878fbe8b423dba2e2de2
enkh <enkh.mon@gmail.com>
2019-11-17 05:25:18 +0800

    insert done

--- 2019-11-17/game-sql.sql
+++ 2019-11-17/game-sql.sql
@@ -1,13 +1,17 @@
 CREATE TABLE teams (
-    id integer not null primary key,
+    id integer not null primary key AUTO_INCREMENT,
     name varchar(255)
 );
 
 CREATE TABLE games (
-    id integer not null primary key,
+    id integer not null primary key AUTO_INCREMENT,
     team1_id integer,
     team2_id integer,
     team1_pts integer,
     team2_pts integer,
     game_date date
 );
+
+
+ALTER TABLE `teams` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `games` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT;
--- 2019-11-17/point-gen.py
+++ 2019-11-17/point-gen.py
@@ -1,8 +1,47 @@
-# import mysql.connector
+import mysql.connector
 import random
 
 teams = []
 counter = 1
+game_counter = 1
+
+mysqlConn = mysql.connector.connect(
+    host="localhost", # 192.168.1.155
+    user="root",
+    database="class_db",
+    passwd=""
+)
+
+def executeSql(query):
+    mysqlCursor = mysqlConn.cursor()
+
+    mysqlCursor.execute(query)
+
+    if(mysqlCursor.with_rows):
+        data = mysqlCursor.fetchall()
+        return data
+    
+    return "OK"
+
+def insertTeam(id, name):
+    insertQuery = '''
+        INSERT INTO teams (id, name) 
+        VALUES
+        (%d, "%s")
+    '''%(id, name)
+    
+    value = executeSql(insertQuery)
+
+def insertGame(data):
+    global game_counter
+    game_counter = game_counter + 1
+    insertQuery = '''
+        INSERT INTO `games`(`team1_id`, `team2_id`, `team1_pts`, `team2_pts`, `game_date`) 
+        VALUES 
+        (%d,%d,%d,%d,%d,"%s")
+    '''%(data[0],data[1],data[2],data[3],data[4])
+    
+    value = executeSql(insertQuery)
 
 def getGame():
     team1_id = random.randrange(1, len(teams)+1)
@@ -37,9 +76,9 @@ for aimag_ner in aimags:
         counter = counter + 1
 
 print(teams)
-# TODO: save teams to DB
-
+for t in teams:
+    insertTeam(t["id"], t["name"])
 
 for i in range(30):
-    print(str(i)+" " + str(getGame()))
-    # TODO: save to DB
+    data = getGame()
+    insertGame(data)
