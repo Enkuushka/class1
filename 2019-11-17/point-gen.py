63ba1b172cd651d8df20036358760b02a13d85a0
enkh <enkh.mon@gmail.com>
2019-11-17 04:40:11 +0800

    task 3

---
+++ 2019-11-17/point-gen.py
@@ -0,0 +1,10 @@
+import mysql.connector
+import random
+
+aimags = ["Arkhangai", "Bulgan", "Zavkhan", "Gobi-Altai"]
+specs = ["Shonkhoruud", "Burgeduud", "Avarguud"]
+
+# Даалгавар: 
+# Дээрх aimags, specs-ийн утгуудыг ашиглаад
+# Arkhangai burgeduud, Bulgan Avarguud гэх мэт
+# нэрсийг үүсгэнэ үү. Бүх боломжоор.
