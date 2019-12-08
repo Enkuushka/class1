e205582e4fc1f5fcef4c08f77bfda40a63925e33
enkh <enkh.mon@gmail.com>
2019-12-08 11:55:39 +0800

    hereglegch burtgeh heseg

--- myweb/hereglegch/urls.py
+++ myweb/hereglegch/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='hereglegch.index'),
+    path('burtgel', views.burtgel, name='burtgel'),
 ]
 
--- myweb/hereglegch/views.py
+++ myweb/hereglegch/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 def index(request):
     return render(request, 'hereglegch/index.html')
+
+def burtgel(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if(form.is_valid()):
+            newUser = User.objects.create_user(form.cleaned_data['username'], '', form.cleaned_data['password1'])
+            newUser.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'hereglegch/burtgel.html', {'form': form})
